c8ee9b65240409fbddbf4a50720defc57fe03b1a
Han Tuzun <han@sofa.hantuzun.com>
2023-02-15 20:34:09 +0300

    this.at is not a function errror
            return convertToRGBA(this.at(x, y), this.palette);
                                      ^

--- packages/ordinouns/README.md
+++ packages/ordinouns/README.md
@@ -1,9 +1,36 @@
 # ordinouns
 
 ## install
+
+### At the root
+
+#### Install Python 3.10
 ```
-npm install
+brew update
+brew install pyenv
+sudo rm -rf /Library/Frameworks/Python.framework
+brew doctor
+brew cleanup
+pyenv install 3.10
+pyenv local 3.10
 ```
+
+### Install node 16.13.1
+```
+n 16.13.1
+```
+
+```
+yarn
+yarn build
+```
+
+#### At root directory
+```
+yarn
+yarn build
+```
+
 
 ```
 npx ts-node index.ts
--- packages/ordinouns/index.ts
+++ packages/ordinouns/index.ts
@@ -6,12 +6,13 @@ import {
     NounsTokenABI,
     PNGCollectionEncoder
 } from '@nouns/sdk';
-// import { readPngFile } from 'node-libpng';
+import { readPngFile } from 'node-libpng';
 import { promises as fs } from 'fs';
-// import path from 'path';
+import * as path from 'path';
 
 const RPC_URL = 'https://mainnet.infura.io/v3/145ce7b859284acb90fd94046cc16dd1';
 
+const imagesDirectory = '../nouns-assets/images/v0';
 const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
 
 const contractAddresses = getContractAddressesForChainOrThrow(ChainId.Mainnet);
@@ -20,21 +21,22 @@ const { nounsTokenContract } = getContractsForChainOrThrow(ChainId.Mainnet, prov
 
 console.log(nounsTokenContract);
 
-// const DESTINATION = path.join(__dirname, './output/image-data.json');
+const DESTINATION = path.join(__dirname, './output/image-data.json');
 
-// const encode = async () => {
-//   const encoder = new PNGCollectionEncoder();
+const encode = async () => {
+  const encoder = new PNGCollectionEncoder();
 
-//   const folders = ['bodies', 'accessories', 'heads', 'glasses'];
-//   for (const folder of folders) {
-//     const folderpath = path.join(__dirname, './images', folder);
-//     const files = await fs.readdir(folderpath);
-//     for (const file of files) {
-//       const image = await readPngFile(path.join(folderpath, file));
-//       encoder.encodeImage(file.replace(/\.png$/, ''), image, folder);
-//     }
-//   }
-//   await encoder.writeToFile(DESTINATION);
-// };
+  const folders = ['1-bodies', '2-accessories', '3-heads', '4-glasses'];
+  for (const folder of folders) {
+    const folderpath = path.join(__dirname, imagesDirectory, folder);
+    const files = await fs.readdir(folderpath);
+    for (const file of files) {
+      const image = await readPngFile(path.join(folderpath, file));
+      console.log(image)
+      encoder.encodeImage(file.replace(/\.png$/, ''), image, folder);
+    }
+  }
+  await encoder.writeToFile(DESTINATION);
+};
 
-// encode();
+encode();
